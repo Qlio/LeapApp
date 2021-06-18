e94b61d6ce2f570581b3b532857f32c16eb0fb30
Qlio <mr.unagaldai@gmail.com>
2021-06-18 10:24:45 +0800

    aeou

--- functions/index.js
+++ functions/index.js
@@ -6,32 +6,69 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 admin.initializeApp()
 
-const baseUrl = 'https://us-central1-qr-payment-demo-6a1fe.cloudfunctions.net/somefunction'
+const BaseUrl = 'https://us-central1-qr-payment-demo-6a1fe.cloudfunctions.net'
+
+// Shop section
 exports.shop_create_invoice = functions.https.onRequest((request, response) => {
     cors(request, response, async () => {
-        const db = admin.firestore()
-        const invoice = await db.collection('invoices').add({
-            name: 'some name',
-        });
+        const db = admin.firestore();
+        const invoice = await db.collection('invoices').add(request.body);
 
-        axios.post('', {
-            invoiceId: invoice.id
+        const { qr } = (
+            await axios.post(`${ BaseUrl }/bank_qr_generate`, {
+                invoiceId: invoice.id,
+                price: request.body.price,
+                callback: `${ BaseUrl }/shop_callback`,
+            })
+        ).data;
+
+        response.send({
+            qr,
+            invoiceId: invoice.id,
         })
     })
 });
 
-exports.somefunction = functions.https.onRequest(async (request, response) => {
-    const db = admin.firestore()
-    const { invoiceId } = request.query
-    functions.logger.log('invoiceId', invoiceId)
+exports.shop_callback = functions.https.onRequest(async (request, response) => {
+    const { invoiceId } = request.body;
+    const db = admin.firestore();
 
     await db.doc(`invoices/${ invoiceId }`).set({
+        status: 'paid',
+    }, { merge: true })
+
+    response.send('successful')
+})
+
+
+// Bank section
+exports.bank_qr_generate = functions.https.onRequest(async (request, response) => {
+    const db = admin.firestore();
+    const { invoiceId, price, callback } = request.body;
+
+    await db.doc(`bank-invoices/${ invoiceId }`).set({ price, callback })
+
+    qrcode.toDataURL(`${ BaseUrl }/bank_callback?invoiceId=${ invoiceId }`, (err, code) => {
+        if(err) return console.log('error occurred')
+
+        response.send({ qr: code });
+    })
+});
+
+exports.bank_callback = functions.https.onRequest(async (request, response) => {
+    const db = admin.firestore()
+    const { invoiceId } = request.query
+
+    const invoice = await db.doc(`bank-invoices/${ invoiceId }`).get()
+    const { callback: callbackUrl } = invoice.data()
+
+    await axios.post(callbackUrl, { invoiceId })
+
+    await db.doc(`bank-invoices/${ invoiceId }`).set({
         status: 'paid',
     }, {
         merge: true,
     })
 
-    response.send({
-        status: 'succes'
-    })
+    response.send('<h1>Successfully paid! Please go to shop.</h1>')
 });
--- src/App.js
+++ src/App.js
@@ -5,30 +5,31 @@ import './App.css';
 import { useState } from 'react';
 
 const BaseUrl = 'https://us-central1-qr-payment-demo-6a1fe.cloudfunctions.net';
-// const BaseUrl = 'http://localhost:5001/qr-payment-demo-6a1fe/us-central1';
 function App() {
     const [ qrImage, setQrImage ] = useState();
     const [ isPaid, setIsPaid ] = useState(false);
+    const [ order, setOrder ] = useState({});
 
     const onButtonClick = async () => {
-        const response = await axios.post(`${ BaseUrl }/shop_create_invoice`, {
-            price: 5000,
-            description: 'blabla',
-            name: 'blabla'
-        })
+        const response = await axios.post(`${ BaseUrl }/shop_create_invoice`, order)
         const { qr, invoiceId } = response.data
 
         setQrImage(qr)
 
-        console.log("Invoice id", invoiceId)
         firestore.doc(`invoices/${ invoiceId }`).onSnapshot((doc) => {
-            console.log("Is doc exists", doc.exists, doc.id)
             if (doc.data().status === 'paid') {
                 setIsPaid(true);
             }
         });
+    }
 
-        console.log(qr)
+    const handleInputChange = (fieldName) => {
+        return (e) => {
+            setOrder({
+                ...order,
+                [fieldName]: e.target.value,
+            })
+        }
     }
 
     return (
@@ -44,6 +45,17 @@ function App() {
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
+                <form>
+                    <div>
+                        <input type="text" placeholder="Барааны нэр" onChange={ handleInputChange('name') }/>
+                    </div>
+                    <div>
+                        <input type="number" placeholder="Үнэ" onChange={  handleInputChange('price') }/>
+                    </div>
+                    <div>
+                        <textarea placeholder="Тайлбар"onChange={  handleInputChange('description') }></textarea>
+                    </div>
+                </form>
                 <button onClick={ onButtonClick }>Click me</button>
             </header>
         </div>
